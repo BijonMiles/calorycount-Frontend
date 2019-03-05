5ef602ec79028f63b9e71b6396388a45c633ee2a
Bijon Miles <bijonmiles01@gmail.com>
2019-03-04 21:58:11 -0500

    checking

--- src/components/Dinnerlist.js
+++ src/components/Dinnerlist.js
@@ -5,7 +5,7 @@ const Dinner = (props) => {
   // console.log(props);
 
   return ( <div>
-      <ul> {props.food.food_name ? props.food.food_name : null }, Calories: {props.food.nf_calories ? props.food.nf_calories : null} |<span className="x-button" onClick={(e) => props.handleListDel(e, props.food, "dinner")}>x</span>| </ul>
+      <ul> {props.food.food_name ? props.food.food_name : null }, Cal: {props.food.nf_calories ? props.food.nf_calories : null} |<span className="x-button" onClick={(e) => props.handleListDel(e, props.food, "dinner")}>x</span>| </ul>
     </div>
   )
 }
--- src/components/Lunchlist.js
+++ src/components/Lunchlist.js
@@ -5,7 +5,7 @@ const Lunchlist = (props) => {
   // console.log(props);
 
   return ( <div>
-      <ul> {props.food.food_name}, Calories: {props.food.nf_calories} |<span className="x-button" onClick={(e) => props.handleListDel(e, props.food, "lunch")}>x</span>| </ul>
+      <ul> {props.food.food_name}, Cal: {props.food.nf_calories} |<span className="x-button" onClick={(e) => props.handleListDel(e, props.food, "lunch")}>x</span>| </ul>
     </div>
   )
 }
