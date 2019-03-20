bd0805f0d585086ab3f8bdcd158f032dbe3d428a
Bijon Miles <bijonmiles01@gmail.com>
2019-03-19 23:25:59 -0400

    update

--- src/components/Dinnerlist.js
+++ src/components/Dinnerlist.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Dinner = (props) => {
 
-  // console.log(props);
+  
 
   return ( <div>
       <ul> {props.food.food_name ? props.food.food_name : null }, Cal: {props.food.nf_calories ? props.food.nf_calories : null} |<span className="x-button" onClick={(e) => props.handleListDel(e, props.food, "dinner")}>x</span>| </ul>
